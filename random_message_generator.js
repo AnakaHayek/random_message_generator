dee10c5c85517d31eb03472977fedf80ba18437e
Hailey Bruckman <haileybruckman@Haileys-MacBook-Pro.local>
2024-06-24 15:38:48 -0500

    Adds randomized compliment body assignment and concatenates the variables into a string

--- random_message_generator.js
+++ random_message_generator.js
@@ -1,4 +1,4 @@
-let compliment = [];
+let compliment = '';
 let subjectNounArr = ['You', 'I', 'My Baby'];
 let verb = '';
 let complimentBodyArr = [['a captivating smile', 'twinkling eyes', 'a contagious smile', 'a brilliant mind', 'the most colorful imagination', 'an extremely soothing voice', 'a beautiful soul'], ['the best at giving hugs', 'a great kisser', 'an inspiration to others', 'the smartest person in the room', 'full of love and joy', 'so patient and kind', 'creative and inquisitive']];
@@ -7,7 +7,7 @@ let subjectNoun;
 let complimentBody;
 
 const randomIndexGen = (arr) => {
-    randomIndex = Math.floor(Math.random() * (arr.length + 1));
+    randomIndex = Math.floor(Math.random() * (arr.length));
 }
 
 randomIndexGen(subjectNounArr);
@@ -33,10 +33,19 @@ if (subjectNoun === 'You') {
     }
 }
 
-if (verb === 'have' || verb === 'has') {
-    randomIndex = randomIndexGen(complimentBodyArr[0]);
-    complimentBody = complimentBodyArr[0][randomIndex];
-} else if (verb === 'are' || verb === 'am' || verb === 'is') {
-    randomIndex = randomIndexGen(complimentBodyArr[1]);
-    complimentBody = complimentBodyArr[1][randomIndex];
-}
+
+ let i;
+ if (verb === 'have' || verb === 'has') {
+       i = Math.floor(Math.random() * (complimentBodyArr[0].length));
+        complimentBody = complimentBodyArr[0][i];
+  } else if (verb === 'are' || verb === 'am' || verb === 'is') {
+        i = Math.floor(Math.random() * (complimentBodyArr[1].length));
+        complimentBody = complimentBodyArr[1][i];
+  } 
+
+
+
+
+
+compliment = subjectNoun + ' ' +  verb + ' ' + complimentBody + '!';
+console.log(compliment);
